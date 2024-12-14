3b3cfbca140c4cde8e9bc7611f49cbc3ccd40b6e
Juskowiiak <juskoh_96@hotmail.com>
2024-12-14 11:23:49 +0000

    KattyBraids - Nav/Main - Estrutura / Responsivo

---
+++ java/Menu.js
@@ -0,0 +1,10 @@
+//abrir Menu
+function abrirMenu() {
+  const navegador = document.querySelector(".navegador");
+  navegador.classList.toggle("closeMenu");
+}
+//fechar Menu
+function fecharMenu() {
+  const navegador = document.querySelector(".navegador");
+  navegador.classList.toggle("closeMenu");
+}
