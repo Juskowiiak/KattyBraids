b7dab9fbc0f10072efb315b471953f4130d85e27
Juskowiiak <juskoh_96@hotmail.com>
2024-12-15 12:49:11 +0000

    Gallery / Retest Responsive - CloseOpen Gallery

---
+++ java/Gallery.js
@@ -0,0 +1,10 @@
+//abrir Menu
+function abrirGallery() {
+  const gallery = document.querySelector(".gallery-photo");
+  gallery.classList.toggle("galleryOpen");
+}
+//fechar Menu
+function fecharGallery() {
+  const gallery = document.querySelector(".gallery-photo");
+  gallery.classList.toggle("galleryOpen");
+}
