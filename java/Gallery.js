4643be6c1211427b6ee34624ee743245ba6f945b
Juskowiiak <juskoh_96@hotmail.com>
2024-12-15 18:31:09 +0000

    FOOTER CRIADO / ADJUST RESPONSIVE

--- java/Gallery.js
+++ java/Gallery.js
@@ -2,9 +2,11 @@
 function abrirGallery() {
   const gallery = document.querySelector(".gallery-photo");
   gallery.classList.toggle("galleryOpen");
+  document.body.style.overflowY = "hidden";
 }
 //fechar Menu
 function fecharGallery() {
   const gallery = document.querySelector(".gallery-photo");
   gallery.classList.toggle("galleryOpen");
+  document.body.style.overflowY = "scroll";
 }
--- java/Menu.js
+++ java/Menu.js
@@ -2,9 +2,11 @@
 function abrirMenu() {
   const navegador = document.querySelector(".navegador");
   navegador.classList.toggle("closeMenu");
+  document.body.style.overflowY = "hidden";
 }
 //fechar Menu
 function fecharMenu() {
   const navegador = document.querySelector(".navegador");
   navegador.classList.toggle("closeMenu");
+  document.body.style.overflowY = "scroll";
 }
