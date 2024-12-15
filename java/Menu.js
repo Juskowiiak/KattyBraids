fc7681587c1f354939db79fc7a1f44e4920ec988
Juskowiiak <juskoh_96@hotmail.com>
2024-12-15 21:23:35 +0000

    Header - Color / Responsive

--- java/Menu.js
+++ java/Menu.js
@@ -10,3 +10,12 @@ function fecharMenu() {
   navegador.classList.toggle("closeMenu");
   document.body.style.overflowY = "scroll";
 }
+
+//vai ajustado a tela
+window.addEventListener("resize", ajustar);
+
+function ajustar() {
+  if (window.outerWidth < 900) {
+    document.body.style.overflowY = "scroll";
+  }
+}
