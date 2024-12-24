8ec02c0eea56757bc92b4dde760342dc2d28f27c
Juskowiiak <juskoh_96@hotmail.com>
2024-12-24 22:16:17 +0000

    Correcoes

--- java/Menu.js
+++ java/Menu.js
@@ -4,11 +4,6 @@ function abrirMenu() {
   navegador.classList.toggle("closeMenu");
   document.body.style.overflowY = "hidden";
 }
-//fechar Menu
-function fecharMenu() {
-  navegador.classList.toggle("closeMenu");
-  document.body.style.overflowY = "scroll";
-}
 
 //vai ajustado a tela
 window.addEventListener("resize", ajustar);
